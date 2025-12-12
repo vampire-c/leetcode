8a414c196381527318e844ebf299244664376bd4
vampire_c <1477437516@qq.com>
2025-12-12 10:33:00 +0800

    Create Solution0283.java

---
+++ everyday/Solution0283.java
@@ -0,0 +1,21 @@
+package leetcode.everyday;
+
+class Solution0283 {
+
+    public static void main(String[] args) {
+        // new Solution0283().moveZeroes(new int[]{0, 1, 0, 3, 12, 0, 1, 0, 1});
+        new Solution0283().moveZeroes(new int[]{1, 1});
+    }
+
+    public void moveZeroes(int[] nums) {
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j] = t;
+                j++;
+            }
+        }
+        System.out.println(nums);
+    }
+}
