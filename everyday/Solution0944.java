fa34d01595b957f49cf4de058fb3ed9dcbac81ea
vampire_c <1477437516@qq.com>
2025-12-22 14:41:01 +0800

    Create Solution0944.java

---
+++ everyday/Solution0944.java
@@ -0,0 +1,26 @@
+package leetcode.everyday;
+
+class Solution0944 {
+
+    public static void main(String[] args) {
+        new Solution0944().minDeletionSize(new String[]{"cba", "daf", "ghi"});
+    }
+
+    public int minDeletionSize(String[] strs) {
+
+        int l = strs.length;
+        int m = strs[0].length();
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < l; j++) {
+                if (strs[j - 1].charAt(i) > strs[j].charAt(i)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
