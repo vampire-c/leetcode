7de5d0d1eaa48383f466659d4e01310dacde9bee
vampire_c <1477437516@qq.com>
2025-12-25 14:57:22 +0800

    Create Solution3075.java

---
+++ everyday/Solution3075.java
@@ -0,0 +1,25 @@
+package leetcode.everyday;
+
+import java.util.Arrays;
+
+class Solution3075 {
+
+    public static void main(String[] args) {
+        long maximumHappinessSum = new Solution3075().maximumHappinessSum(new int[]{1, 2, 3}, 2);
+        System.out.println(maximumHappinessSum);
+    }
+
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int l = happiness.length;
+        long ans = 0;
+        for (int i = 0; i < k; i++) {
+            int t = happiness[--l] - i;
+            if (t == 0) {
+                return ans;
+            }
+            ans += Math.max(t, 0);
+        }
+        return ans;
+    }
+}
