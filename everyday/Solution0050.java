b5bc3b24c3a09cebdf210298e77359eec1a311a2
vampire_c <1477437516@qq.com>
2026-01-13 14:08:22 +0800

    Create Solution0050.java

---
+++ everyday/Solution0050.java
@@ -0,0 +1,26 @@
+package leetcode.everyday;
+
+class Solution0050 {
+
+    public static void main(String[] args) {
+        double v = new Solution0050().myPow(2.1, 3);
+        System.out.println(v);
+    }
+
+    public double myPow(double x, int n) {
+        double ans = 1;
+        long tmp = n;
+        if (tmp < 0) {
+            tmp = -tmp;
+            x = 1 / x;
+        }
+        while (tmp != 0) {
+            if ((tmp & 1) == 1) {
+                ans = ans * x;
+            }
+            x = x * x;
+            tmp = tmp >> 1;
+        }
+        return ans;
+    }
+}
