46cccebc9fcf3c58601ec676c9ac390ebd3ff0d9
vampire_c <1477437516@qq.com>
2026-01-13 14:21:19 +0800

    Create Solution3652.java

---
+++ everyday/Solution3652.java
@@ -0,0 +1,27 @@
+package leetcode.everyday;
+
+class Solution3652 {
+
+    public static void main(String[] args) {
+        long maxProfit = new Solution3652().maxProfit(new int[]{4, 2, 8}, new int[]{-1, 0, 1}, 2);
+        System.out.println(maxProfit);
+    }
+
+    public long maxProfit(int[] prices, int[] strategy, int k) {
+        int n = prices.length;
+        long[] profitSum = new long[n + 1];
+        long[] priceSum = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            profitSum[i + 1] = profitSum[i] + (long)prices[i] * strategy[i];
+            priceSum[i + 1] = priceSum[i] + prices[i];
+        }
+        long res = profitSum[n];
+        for (int i = k - 1; i < n; i++) {
+            long leftProfit = profitSum[i - k + 1];
+            long rightProfit = profitSum[n] - profitSum[i + 1];
+            long changeProfit = priceSum[i + 1] - priceSum[i - k / 2 + 1];
+            res = Math.max(res, leftProfit + changeProfit + rightProfit);
+        }
+        return res;
+    }
+}
