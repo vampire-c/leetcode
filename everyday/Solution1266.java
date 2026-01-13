31bdf24d616b1eb21f16cd5ead53d6d8f77318b4
vampire_c <1477437516@qq.com>
2026-01-13 14:14:26 +0800

    Create Solution1266.java

---
+++ everyday/Solution1266.java
@@ -0,0 +1,11 @@
+package leetcode.everyday;
+
+class Solution1266 {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int ans = 0;
+        for (int i = 0; i < points.length; i++) {
+            ans += Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
+        }
+        return ans;
+    }
+}
