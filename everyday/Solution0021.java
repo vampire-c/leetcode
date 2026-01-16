82c48aa67b29be519585b814c2bdb28f49084551
vampire_c <1477437516@qq.com>
2026-01-16 15:03:35 +0800

    Create Solution0021.java

---
+++ everyday/Solution0021.java
@@ -0,0 +1,41 @@
+package leetcode.everyday;
+
+import leetcode.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution0021 {
+
+    public static void main(String[] args) {
+        new Solution0021().mergeTwoLists(
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(4))),
+                new ListNode(1,
+                        new ListNode(3,
+                                new ListNode(4)))
+        );
+
+
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val < list2.val) {
+            return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
+        } else {
+            return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
+        }
+    }
+}
