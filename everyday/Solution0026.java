ff21ef8dc6b057eaec0a9b7e63ddb2f86cff2a76
vampire_c <1477437516@qq.com>
2026-01-16 16:28:25 +0800

    Create Solution0026.java

---
+++ everyday/Solution0026.java
@@ -0,0 +1,21 @@
+package leetcode.everyday;
+
+class Solution0026 {
+
+    public static void main(String[] args) {
+        new Solution0026().removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4});
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        int left = 0, right = 0;
+        while (right < length ) {
+            if (nums[left] != nums[right]) {
+                nums[++left] = nums[right];
+            }
+            right++;
+        }
+        return ++left;
+
+    }
+}
