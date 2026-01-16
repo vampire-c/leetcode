74bc3ac7fe8cb061e94a350dc211373fa7bb2f83
vampire_c <1477437516@qq.com>
2026-01-16 17:49:16 +0800

    Create Solution0035.java

---
+++ everyday/Solution0035.java
@@ -0,0 +1,31 @@
+package leetcode.everyday;
+
+public class Solution0035 {
+    public static void main(String[] args) {
+        new Solution0035().searchInsert(new int[]{1, 2, 3, 4, 6}, 5);
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return left;
+    }
+
+    public int searchInsert1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
