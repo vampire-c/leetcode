52f76816703e80e8afc1d346dc881beae8a7c0a5
vampire_c <1477437516@qq.com>
2026-01-16 13:47:40 +0800

    Create Solution0014.java

---
+++ everyday/Solution0014.java
@@ -0,0 +1,25 @@
+package leetcode.everyday;
+
+public class Solution0014 {
+
+    public static void main(String[] args) {
+        new Solution0014().longestCommonPrefix(new String[]{"flower", "flow", "flight"});
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
