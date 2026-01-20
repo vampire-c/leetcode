087eaef57fa05fb26c877565ce110a0c23a07a0b
vampire_c <1477437516@qq.com>
2026-01-20 14:22:33 +0800

    Create Solution0066.java

---
+++ everyday/Solution0066.java
@@ -0,0 +1,47 @@
+package leetcode.everyday;
+
+public class Solution0066 {
+
+    public static void main(String[] args) {
+        new Solution0066().plusOne(new int[]{9, 9, 9});
+    }
+
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        for (int i = l - 1; i >= 0; i--) {
+            if (digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits = new int[l + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    public int[] plusOne1(int[] digits) {
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1]++;
+            return digits;
+        }
+
+        boolean p1 = true;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9 && p1) {
+                digits[i] = 0;
+            } else if (digits[i] != 9 && p1) {
+                digits[i]++;
+                p1 = false;
+                break;
+            }
+        }
+        if (digits[0] == 0) {
+            int[] ans = new int[digits.length + 1];
+            ans[0] = 1;
+            System.arraycopy(digits, 0, ans, 1, digits.length);
+            return ans;
+        }
+        return digits;
+    }
+}
