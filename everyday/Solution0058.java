c3e267ebfb910fcdb3868353594685a73fa187c9
vampire_c <1477437516@qq.com>
2026-01-20 13:40:33 +0800

    Create Solution0058.java

---
+++ everyday/Solution0058.java
@@ -0,0 +1,26 @@
+package leetcode.everyday;
+
+class Solution0058 {
+
+    public static void main(String[] args) {
+        new Solution0058().lengthOfLastWord("HelloWorld");
+    }
+
+    public int lengthOfLastWord(String s) {
+        char[] ca = s.toCharArray();
+        int i = ca.length - 1;
+        while (ca[i] == ' ') {
+            i--;
+        }
+        int j = i - 1;
+        while (j >= 0 && ca[j] != ' ') {
+            j--;
+        }
+        return i - j;
+    }
+
+    public int lengthOfLastWord1(String s) {
+        String[] split = s.split(" ");
+        return split[split.length - 1].length();
+    }
+}
