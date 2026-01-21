5ffa4f63b1455a85584fb57b7488bd3e13c87e49
vampire_c <1477437516@qq.com>
2026-01-21 13:44:32 +0800

    Create Solution0094.java

---
+++ everyday/Solution0094.java
@@ -0,0 +1,48 @@
+package leetcode.everyday;
+
+import leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution0094 {
+
+    public static void main(String[] args) {
+        new Solution0094().inorderTraversal(
+                new TreeNode(1,
+                        null, new TreeNode(2,
+                        new TreeNode(3), null)
+                )
+        );
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+
+    private void inorder(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
+}
