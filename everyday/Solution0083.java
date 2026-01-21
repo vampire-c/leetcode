f29f9d2b51cc615a3f08bf0662adb7b6f7925095
vampire_c <1477437516@qq.com>
2026-01-21 11:27:18 +0800

    Create Solution0083.java

---
+++ everyday/Solution0083.java
@@ -0,0 +1,25 @@
+package leetcode.everyday;
+
+import leetcode.ListNode;
+
+public class Solution0083 {
+
+    public static void main(String[] args) {
+        new Solution0083().deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))));
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.next.val == cur.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
