9107b05397f6bd84a65af59490a2b25dcaaa4dd1
vampire_c <1477437516@qq.com>
2026-01-21 14:04:36 +0800

    Create Solution0100.java

---
+++ everyday/Solution0100.java
@@ -0,0 +1,27 @@
+package leetcode.everyday;
+
+import leetcode.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution0100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
