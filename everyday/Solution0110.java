7928103fb417f0ab2f86b4b9122b97e3034ec0fb
vampire_c <1477437516@qq.com>
2026-01-21 17:04:54 +0800

    Create Solution0110.java

---
+++ everyday/Solution0110.java
@@ -0,0 +1,50 @@
+package leetcode.everyday;
+
+import leetcode.TreeNode;
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution0110 {
+    public static void main(String[] args) {
+        new Solution0110().isBalanced(
+                new TreeNode(3,
+                        new TreeNode(9),
+                        new TreeNode(20,
+                                new TreeNode(15),
+                                new TreeNode(7)
+                        )
+                )
+        );
+    }
+
+
+    public boolean isBalanced(TreeNode root) {
+        int height = getHeight(root);
+        return height != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
