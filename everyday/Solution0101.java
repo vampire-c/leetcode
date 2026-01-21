34e9394a1ec6840a3a39d0fda1d7a0b1977fadb6
vampire_c <1477437516@qq.com>
2026-01-21 14:22:49 +0800

    Create Solution0101.java

---
+++ everyday/Solution0101.java
@@ -0,0 +1,42 @@
+package leetcode.everyday;
+
+import leetcode.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution0101 {
+
+    public static void main(String[] args) {
+        new Solution0101().isSymmetric(
+                new TreeNode(1,
+                        new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                        new TreeNode(2, new TreeNode(4), new TreeNode(3))
+                )
+        );
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricTree(root.left, root.right);
+    }
+
+    public boolean isSymmetricTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q;
+        }
+        return p.val == q.val && isSymmetricTree(p.left, q.right) && isSymmetricTree(p.right, q.left);
+    }
+
+}
