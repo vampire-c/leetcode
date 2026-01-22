d01e1e011c396c53c30b60d5ba1b2c8895fe3e0f
vampire_c <1477437516@qq.com>
2026-01-22 10:18:14 +0800

    Create Solution0118.java

---
+++ everyday/Solution0118.java
@@ -0,0 +1,26 @@
+package leetcode.everyday;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution0118 {
+
+    static void main(String[] args) {
+        new Solution0118().generate(5);
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>(numRows);
+        ans.add(List.of(1));
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>(i + 1);
+            temp.add(1);
+            for (int j = 1; j < i; j++) {
+                temp.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+            }
+            temp.add(1);
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
