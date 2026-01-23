2c1e126bc47b11e39140a558cf86eaab91fbe1e3
vampire_c <1477437516@qq.com>
2026-01-23 11:58:52 +0800

    Create Solution0141.java

---
+++ everyday/Solution0141.java
@@ -0,0 +1,59 @@
+package leetcode.everyday;
+
+import leetcode.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution0141 {
+
+    static void main(String[] args) {
+
+        ListNode listNode1 = new ListNode(3);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(0);
+        ListNode listNode4 = new ListNode(-4);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode2;
+
+        new Solution0141().hasCycle(listNode1);
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (!set.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
