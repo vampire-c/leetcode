8ec0ee12a506c2ebef80cb83b8268a1d50bab15b
vampire_c <1477437516@qq.com>
2026-01-27 16:41:13 +0800

    Create Solution0206.java

---
+++ everyday/Solution0206.java
@@ -0,0 +1,59 @@
+package leetcode.problems;
+
+import leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution0206 {
+
+    static void main(String[] args) {
+
+        ListNode listNode =
+                new ListNode(1,
+                        new ListNode(2,
+                                new ListNode(3,
+                                        new ListNode(4,
+                                                new ListNode(5)
+
+                                        )
+                                )
+                        )
+                );
+
+
+        ListNode listNode1 = new Solution0206().reverseList(listNode);
+        System.out.println();
+
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public ListNode reverseList1(ListNode head) {
+        ListNode temp = null;
+        while (head != null) {
+            temp = new ListNode(head.val, temp);
+            head = head.next;
+        }
+        return temp;
+    }
+
+}
