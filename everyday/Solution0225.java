6121e3bc12cd0c92727f2d236ab289745267b199
vampire_c <1477437516@qq.com>
2026-01-28 14:15:40 +0800

    Update Solution0225.java

--- everyday/Solution0225.java
+++ everyday/Solution0225.java
@@ -43,6 +43,32 @@ class MyStack {
     }
 }
 
+class MyStack2 {
+    Queue<Integer> queue;
+
+    public MyStack2() {
+        queue = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        queue.offer(x);
+        for (int i = 0; i < queue.size() - 1; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
 
 class Solution0225 {
 
