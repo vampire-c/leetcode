cff7d9ae45732f0e6297f70cbceecd4a52870528
vampire_c <1477437516@qq.com>
2026-01-28 14:34:53 +0800

    Create Solution0226.java

---
+++ everyday/Solution0226.java
@@ -0,0 +1,49 @@
+package leetcode.problems;
+
+import leetcode.TreeNode;
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution0226 {
+
+    static void main(String[] args) {
+        TreeNode treeNode =
+                new TreeNode(4,
+                        new TreeNode(2,
+                                new TreeNode(1),
+                                new TreeNode(3)
+                        ),
+                        new TreeNode(7,
+                                new TreeNode(6),
+                                new TreeNode(9)
+                        )
+                );
+        new Solution0226().invertTree(treeNode);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
