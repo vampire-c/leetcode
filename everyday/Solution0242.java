8979232cc0b150005bd2c6d81b2bdd79cd69c375
vampire_c <1477437516@qq.com>
2026-01-29 13:50:45 +0800

    Create Solution0242.java

---
+++ everyday/Solution0242.java
@@ -0,0 +1,46 @@
+package leetcode.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution0242 {
+
+    static void main(String[] args) {
+        // new Solution0242().isAnagram("anagram", "nagaram");
+        new Solution0242().isAnagram("rat", "car");
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            // if (map.containsKey(c)) {
+            //     map.put(c, map.get(c) + 1);
+            // } else {
+            //     map.put(c, 1);
+            // }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            // if (map.containsKey(c)) {
+            //     map.put(c, map.get(c) - 1);
+            // } else {
+            //     return false;
+            // }
+            // if (map.get(c) == 0) {
+            //     map.remove(c);
+            // }
+            map.put(c, map.getOrDefault(c, 0) - 1);
+            if (map.get(c) < 0) {
+                return false;
+            }
+        }
+        // return map.isEmpty();
+        return true;
+    }
+}
