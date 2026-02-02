a2999836f67b7948a4d0480e7bf5df32ed59eab3
vampire_c <1477437516@qq.com>
2026-02-02 16:41:28 +0800

    Create Solution0345.java

---
+++ everyday/Solution0345.java
@@ -0,0 +1,58 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution0345 {
+
+    static void main(String[] args) {
+        new Solution0345().reverseVowels("leetcode");
+    }
+
+    public String reverseVowels(String s) {
+        char[] ca = s.toCharArray();
+        for (int left = 0, right = ca.length - 1; left < right; ) {
+            if (!isVowel(ca[left])) {
+                left++;
+                continue;
+            }
+            if (!isVowel(ca[right])) {
+                right--;
+                continue;
+            }
+            char temp = ca[left];
+            ca[left] = ca[right];
+            ca[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(ca);
+    }
+
+    public boolean isVowel(char a) {
+        return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ||
+                a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U';
+    }
+
+    public String reverseVowels1(String s) {
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        char[] ca = s.toCharArray();
+        for (int left = 0, right = ca.length - 1; left < right; ) {
+            if (!set.contains(ca[left])) {
+                left++;
+                continue;
+            }
+            if (!set.contains(ca[right])) {
+                right--;
+                continue;
+            }
+            char temp = ca[left];
+            ca[left] = ca[right];
+            ca[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(ca);
+    }
+}
