06241c68d169c62d7c95ad8645e27ed07940931d
vampire_c <1477437516@qq.com>
2026-02-02 17:04:59 +0800

    Create Solution0349.java

---
+++ everyday/Solution0349.java
@@ -0,0 +1,33 @@
+package leetcode.problems;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution0349 {
+
+    static void main(String[] args) {
+        new Solution0349().intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+    }
+
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        return Arrays.stream(nums2).filter(set::contains).toArray();
+    }
+
+
+    public int[] intersection1(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums1) {
+            set.add(i);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i : nums2) {
+            if (set.remove(i)) {
+                ans.add(i);
+            }
+        }
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
