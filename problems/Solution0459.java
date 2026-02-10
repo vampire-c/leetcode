2f0319de46f9d9dbdae484ad374ed2966839e672
vampire_c <1477437516@qq.com>
2026-02-10 14:25:51 +0800

    Create Solution0459.java

---
+++ problems/Solution0459.java
@@ -0,0 +1,75 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+
+class Solution0459 {
+
+    static void main(String[] args) {
+        new Solution0459().repeatedSubstringPattern("abcdabcd");
+        /*
+        abcdabcd
+
+        a bcdabcdabcdabc d
+
+
+
+         */
+    }
+
+    public boolean repeatedSubstringPattern(String s) {
+        return kmp(s + s, s);
+    }
+
+    public boolean kmp(String query, String pattern) {
+        int n = query.length();
+        int m = pattern.length();
+        int[] fail = new int[m];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < m; i++) {
+            int j = fail[i - 1];
+            while (j != -1 && pattern.charAt(j + 1) != pattern.charAt(i)) {
+                j = fail[j];
+            }
+            if (pattern.charAt(j + 1) == pattern.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+        int match = -1;
+        for (int i = 1; i < n - 1; i++) {
+            while (match != -1 && pattern.charAt(match + 1) != query.charAt(i)) {
+                match = fail[match];
+            }
+            if (pattern.charAt(match + 1) == query.charAt(i)) {
+                match++;
+                if (match == m - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean repeatedSubstringPattern2(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 <= n; i++) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; j++) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean repeatedSubstringPattern1(String s) {
+        return (s + s).indexOf(s, 1) != s.length();
+    }
+}
