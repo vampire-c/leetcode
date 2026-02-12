86f7ec23fbf1c86042e79aaa7e8e1886e974aba6
vampire_c <1477437516@qq.com>
2026-02-12 16:24:48 +0800

    Update Solution0448.java

--- problems/Solution0448.java
+++ problems/Solution0448.java
@@ -3,6 +3,10 @@ package leetcode.problems;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*
+给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。
+请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ */
 class Solution0448 {
 
     static void main(String[] args) {
