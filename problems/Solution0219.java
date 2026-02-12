eada089c469bb78c1f917408245d131d87e92412
vampire_c <1477437516@qq.com>
2026-02-12 17:17:13 +0800

    Update Solution0219.java

--- problems/Solution0219.java
+++ problems/Solution0219.java
@@ -5,6 +5,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/*
+给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。
+如果存在，返回 true ；否则，返回 false 。
+ */
 class Solution0219 {
 
     static void main(String[] args) {
