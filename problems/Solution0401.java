cf0dc0b1a9a212fa4e821573321472662f6d57d3
vampire_c <1477437516@qq.com>
2026-02-12 16:41:29 +0800

    Update Solution0401.java

--- problems/Solution0401.java
+++ problems/Solution0401.java
@@ -3,6 +3,10 @@ package leetcode.problems;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+二进制手表顶部有 4 个 LED 代表 小时（0-11），底部的 6 个 LED 代表 分钟（0-59）。
+每个 LED 代表一个 0 或 1，最低位在右侧。
+ */
 class Solution0401 {
 
     static void main(String[] args) {
