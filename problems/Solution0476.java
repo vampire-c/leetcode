0098169f794e03544cf353d620b0815234afdeb4
vampire_c <1477437516@qq.com>
2026-02-12 16:19:49 +0800

    Add explanation for finding integer complement
    Added a comment explaining how to find the complement of an integer in binary.

--- problems/Solution0476.java
+++ problems/Solution0476.java
@@ -1,5 +1,10 @@
 package leetcode.problems;
 
+/*
+对整数的二进制表示取反（0 变 1 ，1 变 0）后，再转换为十进制表示，可以得到这个整数的补数。
+例如，整数 5 的二进制表示是 "101" ，取反后得到 "010" ，再转回十进制表示得到补数 2 。
+给你一个整数 num ，输出它的补数。
+ */
 class Solution0476 {
 
     static void main(String[] args) {
