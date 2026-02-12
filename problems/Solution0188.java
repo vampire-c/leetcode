91e172abf7e18fb079fee143b84044d641605f59
vampire_c <1477437516@qq.com>
2026-02-12 17:26:13 +0800

    Update Solution0188.java

--- problems/Solution0188.java
+++ problems/Solution0188.java
@@ -2,6 +2,12 @@ package leetcode.problems;
 
 import java.util.Arrays;
 
+/*
+给你一个整数数组 prices 和一个整数 k ，其中 prices[i] 是某支给定的股票在第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+也就是说，你最多可以买 k 次，卖 k 次。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ */
 public class Solution0188 {
 
     public static void main(String[] args) {
