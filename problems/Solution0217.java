3787f67862c47f4efb36c18fb3d8532f4b9fddbf
vampire_c <1477437516@qq.com>
2026-02-12 17:17:50 +0800

    Update Solution0217.java

--- problems/Solution0217.java
+++ problems/Solution0217.java
@@ -3,6 +3,9 @@ package leetcode.problems;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ */
 class Solution0217 {
 
     static void main(String[] args) {
