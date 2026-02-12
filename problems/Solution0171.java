ab1d0ab713a2add5f4239912a0c4db74bd5e38f7
vampire_c <1477437516@qq.com>
2026-02-12 17:27:07 +0800

    Update Solution0171.java

--- problems/Solution0171.java
+++ problems/Solution0171.java
@@ -1,5 +1,17 @@
 package leetcode.problems;
 
+/*
+给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回 该列名称对应的列序号 。
+例如：
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ */
 class Solution0171 {
 
     static void main(String[] args) {
