b87039ad3706604ec5e80d10aa9ce07421162218
vampire_c <1477437516@qq.com>
2026-02-12 17:21:28 +0800

    Update Solution0203.java

--- problems/Solution0203.java
+++ problems/Solution0203.java
@@ -11,6 +11,7 @@ import leetcode.ListNode;
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
  */
 class Solution0203 {
 
