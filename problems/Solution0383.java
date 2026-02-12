eb32dd5c86868f3c620ac82b7e2c06ab80963d94
vampire_c <1477437516@qq.com>
2026-02-12 16:45:33 +0800

    Update Solution0383.java

--- problems/Solution0383.java
+++ problems/Solution0383.java
@@ -3,6 +3,11 @@ package leetcode.problems;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+如果可以，返回 true ；否则返回 false 。
+magazine 中的每个字符只能在 ransomNote 中使用一次。
+ */
 class Solution0383 {
 
     static void main(String[] args) {
