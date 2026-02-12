56551d2fbfa6305cdea64b5b731c701cd4fcaa4e
vampire_c <1477437516@qq.com>
2026-02-12 16:34:41 +0800

    Update Solution0409.java

--- problems/Solution0409.java
+++ problems/Solution0409.java
@@ -2,6 +2,10 @@ package leetcode.problems;
 
 import java.util.Arrays;
 
+/*
+给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的 回文串 的长度。
+在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+ */
 class Solution0409 {
 
     static void main(String[] args) {
