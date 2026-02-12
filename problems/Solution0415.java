7f7aa219644124ef4355d74213c7ee059a8fda13
vampire_c <1477437516@qq.com>
2026-02-12 16:29:46 +0800

    Update Solution0415.java

--- problems/Solution0415.java
+++ problems/Solution0415.java
@@ -1,5 +1,9 @@
 package leetcode.problems;
 
+/*
+给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+ */
 class Solution0415 {
 
     static void main(String[] args) {
