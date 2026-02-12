9df56999e0a82c7d387efa32f03c79b8c81b8a82
vampire_c <1477437516@qq.com>
2026-02-12 16:48:00 +0800

    Update Solution0367.java

--- problems/Solution0367.java
+++ problems/Solution0367.java
@@ -1,5 +1,10 @@
 package leetcode.problems;
 
+/*
+给你一个正整数 num 。如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+完全平方数 是一个可以写成某个整数的平方的整数。换句话说，它可以写成某个整数和自身的乘积。
+不能使用任何内置的库函数，如  sqrt 。
+ */
 class Solution0367 {
 
     static void main(String[] args) throws Exception {
