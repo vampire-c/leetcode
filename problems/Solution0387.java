d64b53da768edf4c20fa32d1e94f57e3a92e9363
vampire_c <1477437516@qq.com>
2026-02-12 16:44:39 +0800

    Update Solution0387.java

--- problems/Solution0387.java
+++ problems/Solution0387.java
@@ -3,6 +3,9 @@ package leetcode.problems;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/*
+给定一个字符串 s ，找到 它的第一个不重复的字符，并返回它的索引 。如果不存在，则返回 -1 。
+ */
 class Solution0387 {
 
     public static void main(String[] args) {
