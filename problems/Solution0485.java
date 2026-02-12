bccd359ae0cf23284a770d598e622aac0453a2d0
vampire_c <1477437516@qq.com>
2026-02-12 14:58:22 +0800

    Create Solution0485.java

---
+++ problems/Solution0485.java
@@ -0,0 +1,37 @@
+package leetcode.problems;
+
+class Solution0485 {
+
+    static void main(String[] args) {
+        new Solution0485().findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1});
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                count = 0;
+            } else {
+                count++;
+                ans = Math.max(ans, count);
+            }
+        }
+        return ans;
+    }
+
+    public int findMaxConsecutiveOnes1(int[] nums) {
+        int ans = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+            } else {
+                ans = Math.max(ans, count);
+                count = 0;
+            }
+        }
+
+        return Math.max(ans, count);
+    }
+}
