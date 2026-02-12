284fbbc4ec9f32f30c12bd53526ae32d9e0d51e8
vampire_c <1477437516@qq.com>
2026-02-12 16:48:43 +0800

    Update Solution0350.java

--- problems/Solution0350.java
+++ problems/Solution0350.java
@@ -2,6 +2,11 @@ package leetcode.problems;
 
 import java.util.*;
 
+/*
+给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。
+返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。
+可以不考虑输出结果的顺序。
+ */
 class Solution0350 {
 
     static void main(String[] args) {
