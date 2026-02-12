2ca40c8210e7602116784154539257cadfb1af79
vampire_c <1477437516@qq.com>
2026-02-12 17:08:37 +0800

    Update Solution0228.java

--- problems/Solution0228.java
+++ problems/Solution0228.java
@@ -3,6 +3,14 @@ package leetcode.problems;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+给定一个  无重复元素 的 有序 整数数组 nums 。
+区间 [a,b] 是从 a 到 b（包含）的所有整数的集合。
+返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表 。也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个区间但不属于 nums 的数字 x 。
+列表中的每个区间范围 [a,b] 应该按如下格式输出：
+"a->b" ，如果 a != b
+"a" ，如果 a == b
+ */
 class Solution0228 {
 
     static void main(String[] args) {
