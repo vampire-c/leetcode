ce6f459ac7bd1ca8f3b02e904863ba9c0c5fa6f7
vampire_c <1477437516@qq.com>
2026-02-12 17:07:51 +0800

    Update Solution0231.java

--- problems/Solution0231.java
+++ problems/Solution0231.java
@@ -1,5 +1,9 @@
 package leetcode.problems;
 
+/*
+给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
+如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+ */
 class Solution0231 {
 
     static void main(String[] args) {
