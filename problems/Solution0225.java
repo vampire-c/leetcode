e70c26c3676fa5d387589403a53637939e8b175a
vampire_c <1477437516@qq.com>
2026-02-12 17:13:49 +0800

    Update Solution0225.java

--- problems/Solution0225.java
+++ problems/Solution0225.java
@@ -10,6 +10,12 @@ import java.util.Queue;
  * int param_2 = obj.pop();
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
+请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+实现 MyStack 类：
+void push(int x) 将元素 x 压入栈顶。
+int pop() 移除并返回栈顶元素。
+int top() 返回栈顶元素。
+boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
  */
 class MyStack {
     Queue<Integer> queue1;
