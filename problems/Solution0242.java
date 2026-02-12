bbc301c33e5452833d080aef2e1fcefb32f1675d
vampire_c <1477437516@qq.com>
2026-02-12 17:06:01 +0800

    Update Solution0242.java

--- problems/Solution0242.java
+++ problems/Solution0242.java
@@ -3,6 +3,9 @@ package leetcode.problems;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的 字母异位词。
+ */
 class Solution0242 {
 
     static void main(String[] args) {
