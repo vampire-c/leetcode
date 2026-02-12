76b34b16c787ee97a14b89bb35c475c0d444885a
vampire_c <1477437516@qq.com>
2026-02-12 13:20:43 +0800

    Create Solution3713.java

---
+++ problems/Solution3713.java
@@ -0,0 +1,127 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+class Solution3713 {
+
+    static void main(String[] args) {
+        new Solution3713().longestBalanced("abbac");
+        // new Solution3713().longestBalanced("acebdccbdabcdccabaeeccbabecaaaedabadbbdeeeeecccabdbcdaeebdeeacdcaecccdbdcbaaaedcbcaeaecaecaebebdeeaecabcbeadceaaeeaaabbbcecaabdbaacacdcdcaaaecdcddcbbbabeecbddadadeaddeddddacadceaeadbdcdadcaaccabedbaeeeccbbdddedebaaeeaadbeadbaabedacabbdddedcccacdcdeddbeaacaaaadcddbeddedcea");
+    }
+
+    public int longestBalanced(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+
+        Map<Character, Integer> cntMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            cntMap.put(c, cntMap.getOrDefault(c, 0) + 1);
+        }
+        boolean isBalanced = true;
+        int firstCount = -1;
+        for (Integer count : cntMap.values()) {
+            if (firstCount == -1) {
+                firstCount = count;
+            } else if (count != firstCount) {
+                isBalanced = false;
+                break;
+            }
+        }
+        if (isBalanced) return n;
+
+
+        int ans = 0;
+
+        return ans;
+    }
+
+    public int longestBalanced3(String s) {
+        char[] ca = s.toCharArray();
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int[] cnt = new int[26];
+            int mx = 0, kinds = 0;
+            for (int j = i; j < n; j++) {
+                int k = ca[j] - 'a';
+                if (cnt[k] == 0) {
+                    kinds++;
+                }
+                mx = Math.max(mx, ++cnt[k]);
+                if (mx * kinds == j - i + 1) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public int longestBalanced2(String s) {
+        int l = s.length();
+        char[] ca = s.toCharArray();
+        int ans = 0;
+        for (int i = 0; i < l; i++) {
+            int[] count = new int[26];
+            next:
+            for (int j = i; j < l; j++) {
+                int base = ++count[ca[j] - 'a'];
+                for (int x = 0; x < count.length; x++) {
+                    if (count[x] > 0 && count[x] != base) {
+                        continue next;
+                    }
+                }
+                ans = Math.max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+
+    public int longestBalanced1(String s) {
+        int l = s.length();
+        int ans = 0;
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j <= l; j++) {
+                String substring = s.substring(i, j);
+                if (ans >= substring.length()) {
+                    continue;
+                }
+                int temp = get(substring);
+                ans = Math.max(ans, temp);
+            }
+        }
+        return ans;
+    }
+
+
+    private int get(String s) {
+        int l = s.length();
+        int[] count = new int['z' + 1];
+        Arrays.fill(count, 0);
+        char[] ca = s.toCharArray();
+        boolean flag = true;
+        for (char c : ca) {
+            if (count[c] != 0) {
+                flag = false;
+                break;
+            }
+            count[c]++;
+        }
+        if (flag) {
+            return l;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : ca) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        if (map.size() == 1) return l;
+        Integer value = map.values().iterator().next();
+        boolean b = map.values().stream().allMatch(v -> Objects.equals(v, value));
+        if (b) {
+            return l;
+        }
+        return 0;
+    }
+}
