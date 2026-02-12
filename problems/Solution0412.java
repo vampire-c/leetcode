0ef09f3120ce1a9a61b190aab072c33315460633
vampire_c <1477437516@qq.com>
2026-02-12 16:33:47 +0800

    Add FizzBuzz problem description comment
    Add a comment explaining the FizzBuzz problem requirements.

--- problems/Solution0412.java
+++ problems/Solution0412.java
@@ -3,6 +3,13 @@ package leetcode.problems;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+给你一个整数 n ，返回一个字符串数组 answer（下标从 1 开始），其中：
+answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+answer[i] == "Fizz" 如果 i 是 3 的倍数。
+answer[i] == "Buzz" 如果 i 是 5 的倍数。
+answer[i] == i （以字符串形式）如果上述条件全不满足。
+ */
 class Solution0412 {
 
     static void main(String[] args) {
