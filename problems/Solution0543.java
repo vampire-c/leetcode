dbbe17ec116424ee7700757cd20149fc022bdf5d
vampire_c <1477437516@qq.com>
2026-02-24 16:27:57 +0800

    Update Solution0543.java

--- problems/Solution0543.java
+++ problems/Solution0543.java
@@ -16,6 +16,10 @@ import leetcode.TreeNode;
  *         this.right = right;
  *     }
  * }
+给你一棵二叉树的根节点，返回该树的 直径 。
+二叉树的 直径 是指树中任意两个节点之间最长路径的 长度 。
+这条路径可能经过也可能不经过根节点 root 。
+两节点之0间路径的 长度 由它们之间边数表示。
  */
 class Solution0543 {
 
