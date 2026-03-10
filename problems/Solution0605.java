945f8e9b04de76a12d57bb1a409bc716ad0c10e3
vampire_c <1477437516@qq.com>
2026-03-10 17:07:05 +0800

    Create Solution0605.java

---
+++ problems/Solution0605.java
@@ -0,0 +1,40 @@
+package leetcode.problems;
+
+/*
+假设有一个很长的花坛，一部分地块种植了花，另一部分却没有。
+可是，花不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+给你一个整数数组 flowerbed 表示花坛，由若干 0 和 1 组成，其中 0 表示没种植花，1 表示种植了花。
+另有一个数 n ，能否在不打破种植规则的情况下种入 n 朵花？能则返回 true ，不能则返回 false 。
+ */
+class Solution0605 {
+
+    static void main(String[] args) {
+        new Solution0605().canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1);
+    }
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int length = flowerbed.length;
+        for (int i = 0; i < length; i++) {
+            if ((i == 0 || flowerbed[i - 1] == 0) &&
+                    flowerbed[i] == 0 &&
+                    (i == length - 1 || flowerbed[i + 1] == 0)
+            ) {
+                n--;
+                i++;
+            }
+        }
+        return n <= 0;
+    }
+
+    public boolean canPlaceFlowers1(int[] flowerbed, int n) {
+        int[] ints = new int[flowerbed.length + 2];
+        System.arraycopy(flowerbed, 0, ints, 1, flowerbed.length);
+        for (int i = 1; i < ints.length - 1; i++) {
+            if (ints[i - 1] == 0 && ints[i] == 0 && ints[i + 1] == 0) {
+                ints[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}
