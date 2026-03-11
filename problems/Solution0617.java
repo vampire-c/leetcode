467de26b40b9d2eb21bd42f57ef9d0d46c625989
vampire_c <1477437516@qq.com>
2026-03-11 13:56:43 +0800

    Create Solution0617.java

---
+++ problems/Solution0617.java
@@ -0,0 +1,109 @@
+package leetcode.problems;
+
+import leetcode.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+给你两棵二叉树： root1 和 root2 。
+想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。
+你需要将这两棵树合并成一棵新二叉树。
+合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+返回合并后的二叉树。
+注意: 合并过程必须从两个树的根节点开始。
+ */
+class Solution0617 {
+
+    static void main(String[] args) {
+        TreeNode root1 = new TreeNode(
+                1,
+                new TreeNode(3, new TreeNode(5), null),
+                new TreeNode(2, null, new TreeNode(4))
+        );
+
+        TreeNode root2 = new TreeNode(
+                2,
+                new TreeNode(1, null, new TreeNode(4)),
+                new TreeNode(3, null, new TreeNode(7))
+        );
+
+        new Solution0617().mergeTrees(root1, root2);
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode merged = new TreeNode(root1.val + root2.val);
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue.offer(merged);
+        queue1.offer(root1);
+        queue2.offer(root2);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node = queue.poll(), node1 = queue1.poll(), node2 = queue2.poll();
+            TreeNode left1 = node1.left, left2 = node2.left;
+            TreeNode right1 = node1.right, right2 = node2.right;
+            if (left1 != null || left2 != null) {
+                if (left1 != null && left2 != null) {
+                    TreeNode left = new TreeNode(left1.val + left2.val);
+                    node.left = left;
+                    queue.offer(left);
+                    queue1.offer(left1);
+                    queue2.offer(left2);
+                } else if (left1 != null) {
+                    node.left = left1;
+                } else if (left2 != null) {
+                    node.left = left2;
+                }
+            }
+            if (right1 != null || right2 != null) {
+                if (right1 != null && right2 != null) {
+                    TreeNode right = new TreeNode(right1.val + right2.val);
+                    node.right = right;
+                    queue.offer(right);
+                    queue1.offer(right1);
+                    queue2.offer(right2);
+                } else if (right1 != null) {
+                    node.right = right1;
+                } else if (right2 != null) {
+                    node.right = right2;
+                }
+
+            }
+        }
+        return merged;
+    }
+
+    public TreeNode mergeTrees2(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+
+    public TreeNode mergeTrees1(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        return new TreeNode(
+                root1.val + root2.val,
+                mergeTrees(root1.left, root2.left),
+                mergeTrees(root1.right, root2.right)
+        );
+    }
+}
