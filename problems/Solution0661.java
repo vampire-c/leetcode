5d5d507a80fe80163bd369d30f97058f1d8d08fa
vampire_c <1477437516@qq.com>
2026-03-12 16:47:50 +0800

    Create Solution0661.java

---
+++ problems/Solution0661.java
@@ -0,0 +1,77 @@
+package leetcode.problems;
+
+
+/*
+图像平滑器 是大小为 3 x 3 的过滤器，用于对图像的每个单元格平滑处理，平滑处理后单元格的值为该单元格的平均灰度。
+每个单元格的  平均灰度 定义为：该单元格自身及其周围的 8 个单元格的平均值，结果需向下取整。（即，需要计算蓝色平滑器中 9 个单元格的平均值）。
+如果一个单元格周围存在单元格缺失的情况，则计算平均灰度时不考虑缺失的单元格（即，需要计算红色平滑器中 4 个单元格的平均值）。
+给你一个表示图像灰度的 m x n 整数矩阵 img ，返回对图像的每个单元格平滑处理后的图像 。
+ */
+class Solution0661 {
+
+    static void main(String[] args) {
+        new Solution0661().imageSmoother(new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}});
+    }
+
+    public int[][] imageSmoother(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] sum = new int[m + 10][n + 10];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + img[i - 1][j - 1];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int a = Math.max(0, i - 1), b = Math.max(0, j - 1);
+                int c = Math.min(m - 1, i + 1), d = Math.min(n - 1, j + 1);
+                int cnt = (c - a + 1) * (d - b + 1);
+                int tot = sum[c + 1][d + 1] - sum[a][d + 1] - sum[c + 1][b] + sum[a][b];
+                ans[i][j] = tot / cnt;
+            }
+        }
+        return ans;
+    }
+
+    public int[][] imageSmoother2(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] ans = new int[m][n];
+        int[][] dirs = new int[][]{{0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int tot = 0, cnt = 0;
+                for (int[] dir : dirs) {
+                    int nx = i + dir[0], ny = j + dir[1];
+                    if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                        continue;
+                    }
+                    tot += img[nx][ny];
+                    cnt++;
+                }
+                ans[i][j] = tot / cnt;
+            }
+        }
+        return ans;
+    }
+
+    public int[][] imageSmoother1(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] ret = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = 0, sum = 0;
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        if (x >= 0 && x < m && y >= 0 && y < n) {
+                            num++;
+                            sum += img[x][y];
+                        }
+                    }
+                }
+                ret[i][j] = sum / num;
+            }
+        }
+        return ret;
+    }
+}
