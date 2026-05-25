d5483da04351adc5654b172b41c0649e1d206978
vampire_c <47297161+vampire-c@users.noreply.github.com>
2026-05-25 16:43:49 +0800

    Add problem statement comment to Solution0001.java
    Added a comment block explaining the problem statement for the twoSum method.

--- problems/Solution0001.java
+++ problems/Solution0001.java
@@ -3,7 +3,12 @@ package leetcode.problems;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Solution0001 {
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+你可以按任意顺序返回答案。
+ */
+class Solution0001 {
     public static void main(String[] args) {
         new Solution0001().twoSum(new int[]{2, 7, 11, 15}, 9);
     }
