b0d260a4ec8b1ef259eb8f0719e96444266d3cb2
vampire_c <47297161+vampire-c@users.noreply.github.com>
2026-05-25 18:30:21 +0800

    Update Solution0011.java

--- problems/Solution0011.java
+++ problems/Solution0011.java
@@ -1,18 +1,25 @@
 package leetcode.problems;
 
+/*
+给定一个长度为 n 的整数数组 height。
+有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i])。
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+返回容器可以储存的最大水量。
+说明：你不能倾斜容器。
+ */
 class Solution0011 {
 
     public static void main(String[] args) {
-        System.out.println(new Solution0011().maxArea(new int[]{5, 4, 3, 2, 1, 4, 8, 3, 7}));
+        System.out.println(new Solution0011().maxArea1(new int[]{5, 4, 3, 2, 1, 4, 8, 3, 7}));
     }
 
 
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
         int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
         while (left < right) {
-            int minLine = Math.min(height[left], height[right]);
-            int ares = minLine * (right - left);
+            int ares = Math.min(height[left], height[right]) * (right - left);
             ans = Math.max(ans, ares);
             if (height[left] < height[right]) {
                 while (left < right && height[++left] <= height[left - 1]) ;
@@ -24,8 +31,9 @@ class Solution0011 {
     }
 
     public int maxArea1(int[] height) {
-        int left = 0, right = height.length - 1;
         int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
         while (left < right) {
             int area = Math.min(height[left], height[right]) * (right - left);
             ans = Math.max(ans, area);
